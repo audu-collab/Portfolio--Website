d411aaefca9b812f51000c6646336f1ccef2559c
Audumbar Gutte <11.02.2004.ang@gmail.com>
2024-07-24 01:13:51 +0530

    relocation commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,6 @@
 const nextConfig = {
     // Enabling static export mode
     trailingSlash: true,
-  
-    // Configuring export path map
-    exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-      return {
-        '/': { page: '/' },
-        '/about': { page: '/about' },
-        '/projects': { page: '/projects' },
-        '/contact': { page: '/contact' },
-        // Add additional pages as needed
-      }
-    },
   };
   
   export default nextConfig;
